72fb0c86b019b4254c55c1948b4207b3a8e48765
susumoa <tobiasbori@gmail.com>
2020-03-17 12:41:50 +0000

    feat: add actions, chore: update package

---
+++ src/actions/shared.js
@@ -0,0 +1,17 @@
+import { _getQuestions } from '../utils/_DATA'
+import { receiveUsers } from '../actions/users'
+import { receiveQuestions } from '../actions/questions'
+import { setAuthedUser } from '../actions/authedUser'
+
+const AUTHED_ID = 'johndoe'
+
+export function handleInitialData() {
+  return (dispatch) => {
+    return getQuestions()
+      .then(({ users, questions }) => {
+        dispatch(receiveUsers(users))
+        dispatch(receiveQuestions(questions))
+        dispatch(setAuthedUser(AUTHED_ID))
+      })
+  }
+}
