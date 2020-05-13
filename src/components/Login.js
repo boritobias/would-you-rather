d9b7d7b24dc2cc0695a1f809e3f3f11e44c9af14
susumoa <tobiasbori@gmail.com>
2020-05-13 15:26:40 +0100

    convert login to function component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import { Form, Dropdown, Button, Header } from 'semantic-ui-react'
 import { setAuthedUser } from '../actions/authedUser'
 import AppHeader from './AppHeader'
 
-class Login extends Component {
-  state = {
-    value: ''
+function Login(props) {
+  const [value, setValue] = useState('')
+
+  const onChange = (e, { value }) => {
+    setValue(value)
   }
 
-  onChange = (e, { value }) => {
-    this.setState({ value })
-  }
-
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault()
-    const authedUser = this.state.value
-    this.props.dispatch(setAuthedUser(authedUser))
+    const authedUser = value
+    props.dispatch(setAuthedUser(authedUser))
   }
 
-  dropdownOptions = () => {
-    const { users } = this.props
+  const dropdownOptions = () => {
+    const { users } = props
 
     return users.map((user) => ({
       key: user.id,
@@ -30,26 +28,24 @@ class Login extends Component {
     }))
   }
 
-  render() {
-    const disabled = this.state.value === '' ? true : false
+    const disabled = value === '' ? true : false
 
     return (
       <div>
        <AppHeader />
         <Header textAlign='center'>Log in to use the app</Header>
-        <Form onSubmit={this.handleSubmit} className='login-center'>
+        <Form onSubmit={handleSubmit} className='login-center'>
           <Dropdown
             placeholder='Select a user'
             fluid
             selection
-            options={this.dropdownOptions()}
-            onChange={this.onChange}
+            options={dropdownOptions()}
+            onChange={onChange}
           />
           <Button disabled={disabled}>Log in</Button>
         </Form>
       </div>
     )
-  }
 }
 
 
